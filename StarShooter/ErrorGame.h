b5c8e8186ced41a632dfdb9a7dc18a18b6763a12
Alexis <alexis.aubineau@ynov.com>
2021-03-29 18:47:00 +0200

    Rename player_projectile to ProjectileComponent

---
+++ StarShooter/ErrorGame.h
@@ -0,0 +1,10 @@
+#ifndef ERROR_H
+#define ERROR_H
+#include <SFML/System/String.hpp>
+
+class ErrorGame {
+public:
+	sf::String couldNotLoadTexture = "ERROR::GAME_STATE::COULD_NOT_LOAD_PLAYER_TEXTURE";
+};
+
+#endif // ERROR_H
