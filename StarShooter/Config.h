c9910720773e5284885d10a50569cb5e0e2745e5
Alexis <alexis.aubineau@ynov.com>
2021-03-30 19:40:33 +0200

    some fix

--- StarShooter/Config.h
+++ StarShooter/Config.h
@@ -6,17 +6,19 @@ class Config
 {
 	
 public:
-	//Config file;
+	// Config file;
 	const std::string windowSettingsPath = "Config/window.ini";
 	const std::string keySupportedPath = "Config/supported_keys.ini";
 	const std::string gameStateKeybindingPath = "Config/gamestate_keybinds.ini";
 	const std::string mainMenuKeybindingPath = "Config/mainmenustate_keybinds.ini";
 
-	//Sprites
+	// Sprites
 	const std::string playerIdlePath = "Resources/Images/Sprites/Player/SHIP_IDLE_SHEET.png";
-	const sf::String backgroundPath = "Resources/Images/Backgrounds/bg1.png";
 	const std::string ProjectilePath = "Resources/Images/Sprites/SHIP_PROJECTILE_SHEET.png";
 
+	// Background
+	const sf::String backgroundPath = "Resources/Images/Backgrounds/bg1.png";
+	
 	// Font
 	const sf::String fontPath = "Fonts/Dosis-Light.ttf";
 	
